9a4a659e7fb1d3c4043b1d3d92c3049b82dcc477
Erica Cannedy <encannedy@gmail.com>
2020-04-26 14:10:43 -0400

    updates to hs and index.html. onclick is registering.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-myStorage = window.localStorage;
+var textArea = localStorage.getItem("textArea");
+
+$("<button>")
+
 
 //create save buttons using classes in css
 //create color code green-present grey-past -red present
+
+$(".far fa-save").on("click", function() {
+
+    localStorage.setItem("#myTextArea", textArea);
+});
+
+mystorage.getItem(this);
+
+
+$(document).ready();
+
